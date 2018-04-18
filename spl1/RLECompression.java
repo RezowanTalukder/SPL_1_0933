8dc51e671ca2c7b5af213f95c58ecb0c2b0ead51
RezowanTalukder <35560302+RezowanTalukder@users.noreply.github.com>
2018-04-19 01:41:54 +0600

    Add files via upload

--- spl1/RLECompression.java
+++ spl1/RLECompression.java
@@ -1,8 +1,16 @@
 package rez1;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,51 +20,33 @@ public class RLECompression {
     
         
     
-        public static String encode(String fileName) {
             
-            
-            
-            try{
-                /*
-                byte[] buffer = new byte[10000] ;
-                
-                FileInputStream inp = new FileInputStream(fileName) ;
-                int nRead = 0 ;
-                while(nRead = inp.read(buffer)!=-1){
-                    
-                }
-                
-              */
-                
-                
-                
-                FileReader fileReader = new FileReader(fileName) ;
-                BufferedReader br = new BufferedReader(fileReader) ;
-                String source = null , dest = null ;
-                while((source= br.readLine())!=null){
-                        dest = null ;
-                        //int runLength = 0 ;
-                        for (int i = 0; i < source.length(); i++) {
-                        int runLength = 1 ;
-                        while (i + 1 < source.length()&& source.charAt(i) == source.charAt(i + 1)) {
-                            runLength++ ;
-                            i++ ;
+         public static void encode(String source) {
+                    StringBuffer dest = new StringBuffer();
+                    for (int i = 0; i < source.length(); i++) {
+                        int runLength = 1;
+                        while (i + 1 < source.length()
+                                && source.charAt(i) == source.charAt(i + 1)) {
+                            runLength++;
+                            i++;
                         }
                         dest.append(runLength);
                         dest.append(source.charAt(i));
                     }
-                }
-                
-            }catch(Exception e){
-                
-            }
-            
-            StringBuffer dest = new StringBuffer();
-            
-            return dest.toString();
-        }
+                    //return dest.toString();
+                    try{
+                        System.out.println(dest.toString());
+                        
+                        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("rle.txt",true))) ;
+                        Files.write(Paths.get("rle.txt"),dest.toString().getBytes(), StandardOpenOption.APPEND) ;
+                        //out.println(dest.toString().getBytes());
+                    }catch(Exception e){
+                        //
+                    }
+           }
+     
 
-        public static String decode(String source) {
+        public static void decode(String source) {
             StringBuffer dest = new StringBuffer();
             Pattern pattern = Pattern.compile("[0-9]+|[a-zA-Z]");
             Matcher matcher = pattern.matcher(source);
@@ -67,13 +57,37 @@ public class RLECompression {
                     dest.append(matcher.group());
                 }
             }
-            return dest.toString();
+            //return dest.toString();
+            
+            try{
+                        System.out.println(dest.toString());
+                        //
+                        PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("rlenew.txt",true))) ;
+                        Files.write(Paths.get("rlenew.txt"),dest.toString().getBytes(), StandardOpenOption.APPEND) ;
+                        //out.println(dest.toString().getBytes());
+                    }catch(Exception e){
+                        //
+                    }
         }
 
-        public static void main(String[] args) {
-            String example = "aaaaabbbbbcccccr" ;
-            System.out.println(encode(example));
-            System.out.println(decode(encode(example)));
+        public static void main(String[] args) throws FileNotFoundException, IOException {
+            String example = null ;
+            FileReader in = new FileReader("input.txt") ;
+            BufferedReader br = new BufferedReader(in) ;
+            while((example=br.readLine())!=null){
+                encode(example);
+            }
+            
+           
+             in = new FileReader("rle.txt") ;
+             br = new BufferedReader(in) ;
+            
+            while((example=br.readLine())!=null){
+                decode(example);
+            }
+            
+            
+            
         }
  
 }
