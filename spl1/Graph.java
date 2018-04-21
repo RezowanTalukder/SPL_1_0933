1a1a8f9326a27f49ac47807e9c1f80a7660520ee
RezowanTalukder <35560302+RezowanTalukder@users.noreply.github.com>
2018-04-22 00:49:04 +0600

    Add files via upload

--- spl1/Graph.java
+++ spl1/Graph.java
@@ -1,23 +1,24 @@
 
 package rez1;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
-import static java.awt.Color.ORANGE;
+import static javax.swing.GroupLayout.Alignment.CENTER;
 import org.jfree.chart.ChartFactory;
+import org.jfree.chart.ChartPanel;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.CategoryPlot;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.category.DefaultCategoryDataset;
 
-
 public class Graph extends javax.swing.JFrame {
 
-   
+  
     public Graph() {
         initComponents();
     }
 
-  
+   
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
@@ -26,6 +27,8 @@ public class Graph extends javax.swing.JFrame {
         jPanel1 = new javax.swing.JPanel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setPreferredSize(new java.awt.Dimension(800, 600));
+        getContentPane().setLayout(new javax.swing.BoxLayout(getContentPane(), javax.swing.BoxLayout.LINE_AXIS));
 
         jButton1.setText("SHOW");
         jButton1.addActionListener(new java.awt.event.ActionListener() {
@@ -33,71 +36,43 @@ public class Graph extends javax.swing.JFrame {
                 jButton1ActionPerformed(evt);
             }
         });
+        getContentPane().add(jButton1);
 
+        jPanel1.setBackground(new java.awt.Color(153, 204, 255));
         jPanel1.setLayout(new java.awt.BorderLayout());
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jButton1))
-                .addContainerGap(531, Short.MAX_VALUE))
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jButton1)
-                .addGap(50, 50, 50)
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(331, Short.MAX_VALUE))
-        );
+        getContentPane().add(jPanel1);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        DefaultCategoryDataset bar = new DefaultCategoryDataset() ;
-        bar.setValue(20000," Ratio ", "LZW");
-        bar.setValue(20000," Ratio ", "LZ77");
-        bar.setValue(20000," Ratio ", "RLE");
-        bar.setValue(20000," Ratio ", "Huffman");
         
-        JFreeChart barChart = ChartFactory.createBarChart("Compression Ratio", "Ration", "value", bar, PlotOrientation.VERTICAL, false, true,false) ;
-        CategoryPlot barCh = barChart.getCategoryPlot() ;
-        barCh.setRangeGridlinePaint(Color,ORANGE) ;      
+        double huff = 0.5 ;
+        double lzw = 0.6 ;
+        double lz77 = 0.8 ;
+        double rle = 0.4 ;
+        
+        DefaultCategoryDataset barChartData = new DefaultCategoryDataset() ;
+        barChartData.setValue(huff,"Compression Ratio", "Huffman");
+        barChartData.setValue(lzw,"Compression Ratio", "LZW");
+        barChartData.setValue(rle,"Compression Ratio", "RLE");
+        barChartData.setValue(lz77,"Compression Ratio", "LZ77");
+        
+        JFreeChart barChart = ChartFactory.createBarChart("Compression Test","Algorithms", "Compression Ratio", barChartData,PlotOrientation.VERTICAL,false,true,false) ;
+        
+        CategoryPlot barChrt = barChart.getCategoryPlot() ;
+        barChrt.setRangeGridlinePaint(Color.ORANGE);
+        
+        ChartPanel barPanel = new ChartPanel(barChart) ;
+        jPanel1.removeAll() ;
+        jPanel1.add(barPanel) ;
+        
+        jPanel1.validate();
     }//GEN-LAST:event_jButton1ActionPerformed
 
-    
+   
     public static void main(String args[]) {
-        /* Set the Nimbus look and feel */
-        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
-        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
-         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
-         */
-        try {
-            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
-                if ("Nimbus".equals(info.getName())) {
-                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
-                    break;
-                }
-            }
-        } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Graph.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Graph.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Graph.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Graph.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
-        }
-        //</editor-fold>
-
-        /* Create and display the form */
+       
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Graph().setVisible(true);
